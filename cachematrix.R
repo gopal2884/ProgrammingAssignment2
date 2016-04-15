3f09eebfc708d1d890c49cdd6ee680c4e7fe7b56
Gopal Ravisankar <Gopal28@gmail.com>
2016-04-14 21:40:25 -0400

    MakeCacheMatrix Function is written. First Commit.

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,25 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        inverse <- NULL
+        
+        ## set the matrix
+        
+        set <- function(y) {
+                x <<- y
+                inverse <<- NULL
+        }
+        ## get the matrix
+        get <- function() x
+        
+        ## set and get the cached matrix
+        setcache <- function(inversematrix) inverse <<- matrixinverse
+        getcache <- function() inverse
+        
+        ##this returns the type of the cached matrix
+        list(set = set, get = get,
+             setcache = setcache,
+             getcache = getcache)
 
 }
 
